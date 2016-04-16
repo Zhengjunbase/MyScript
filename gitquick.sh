d9e2459f98e32c937fd93588e306602e40caf716
zhengjun <zhengjunbase@gmail.com>
2016-04-16 11:07:58 +0800

    default msg

--- gitquick.sh
+++ gitquick.sh
@@ -5,6 +5,7 @@ if [ $1 == $empty ]
 	then
 	git commit -m "default msg"
   else
-  		git commit -m "$1 $2 $3 $4 $5 $6 $7 $8 $9 $10"
+  		git commit -m "$1 $2 $3 $4 $5 $6 $7 $8 $9"
 fi
 git push;
+mvn -Pdev -U -e clean package -Dmaven.test.skip=true;
